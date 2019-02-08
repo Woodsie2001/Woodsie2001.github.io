8fd54adf51c4c9465b530b16d2f7a813f4aa346b
Woodsie2001 <connerw306@gmail.com>
2019-02-08 11:00:38 -0600

    random circles
    YASSSSSSSSSSSSSSSSSSS

--- cs30-p5js-template-master/cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/cs30-p5js-template-master/sketch.js
@@ -8,12 +8,8 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rectMode(CENTER);
-
 }
 
 function draw() {
-  background(0);
-  fill(250, 0, 0);
-  rect(mouseX, 200, 400, 400)
+  background(220);
 }
