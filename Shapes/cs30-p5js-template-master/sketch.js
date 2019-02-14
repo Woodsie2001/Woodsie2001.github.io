5ad2041cacfa542167a71d4b4a866616156a5801
Woodsie2001 <connerw306@gmail.com>
2019-02-14 10:34:51 -0600

    Done my Shapes

--- Shapes/cs30-p5js-template-master/sketch.js
+++ Shapes/cs30-p5js-template-master/sketch.js
@@ -1,7 +1,7 @@
-// Project Title
-// Your Name
-// Date
-//
+// Primitive Paint
+// Conner Woods
+// Feb 14th 
+//RAINBOW!!!!!!!!!!!!!!!!!
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
@@ -12,32 +12,44 @@ function setup() {
 }
 
 function draw() {
-  background(255,255,255);
+  //background(255,255,255);
   keyTyped();
   textSize(50);
   textFont("Georgia");
   text("Conner Woods", 10, 50);
   fill(random(255),random(255),random(255));
+  //Creating My Name
+
 }
 
 function keyTyped() {
   if (key === "a") {
     fill(random(255), random(255), random(255));
+    //RAINBOW
     rect(mouseX,mouseY, 150, 200);
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 150, 200);
-    }
+    //Creating Rectangle
+
   }
   if (key === "s"){
     fill(random(255), random(255), random(255));
+    //RAINBOW
     ellipse(mouseX,mouseY,300,200 );
+    //Creating Ellipse
+
   }
   if (key === "d"){
     fill(random(255), random(255), random(255));
+    //RAINBOW
     quad(mouseX, mouseY, mouseX - 25, mouseY + 25, mouseX, mouseY +50, mouseX +25, mouseY + 25);
+    //Creating Quad
+
   }
   if (key === " "){
-    background(0);
+    background(255);
+    //Wiping Screen
+
   }
   triangle(random(0,2000), random(0,1000), random(0,1000), random(0,2000), random(0,1000), random(0,1000));
+  //Creating Random Triangles
+
 }
