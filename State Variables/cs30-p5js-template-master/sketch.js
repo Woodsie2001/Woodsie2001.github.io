66e65c470a69af78157b42259915eb81fbe430ef
Woodsie2001 <connerw306@gmail.com>
2019-02-27 10:59:23 -0600

    Almost Done

--- State Variables/cs30-p5js-template-master/sketch.js
+++ State Variables/cs30-p5js-template-master/sketch.js
@@ -5,21 +5,48 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let quad = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function determinequad(){
+  if (mouseX < width/2){
+    if (mouseY < height/2) quad = 1;
+    else quad = 3;
+  }
+  else{
+    if (mouseY < height/2) quad = 2;
+    else quad = 4;
+  }
+}
+
+function drawSquare(){
+  if (quad === 1){
+    rect(0, 0, width/2, height/2);
+    fill (0);
+  }
+  if (quad === 2){
+    rect(width/2, 0 , width/2, height/2);
+    fill(0);
+  }
+  if (quad === 3){
+    rect(0, height/2, width/2, height/2);
+    fill(0);
+  }
+  if (quad === 4){
+    rect(width/2, height/2, width/2, height/2);
+    fill(0);
+  }
+}
+
 function draw() {
-  background(220);
+  background(255);
   line(0, windowHeight/2, windowWidth, windowHeight/2);
   line(windowWidth/2, 0, windowWidth/2, windowHeight);
-  if (mouseX >= windowWidth/2 && mouseY <= windowHeight/2){
-    quad();
-  }
-
+  determinequad();
+  drawSquare();
 }
 
-function quad(){ 
-  fill(0);
-}
+
