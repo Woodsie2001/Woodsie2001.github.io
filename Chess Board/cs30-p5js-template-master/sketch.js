9d5bd881f924113f09465631533f4b43d4159c37
Woodsie2001 <connerw306@gmail.com>
2019-03-07 10:54:14 -0600

    WORK

--- Chess Board/cs30-p5js-template-master/sketch.js
+++ Chess Board/cs30-p5js-template-master/sketch.js
@@ -5,26 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let colors = 0;
+
+let x;
+let y;
+
 
 function setup() {
-  createCanvas(windowHeight, windowHeight);
+  createCanvas(windowWidth, windowHeight);
   noLoop();
 }
 
 function draw() {
-  for (let x = 0; x <= width; x += width/8){
-    for (let y = 0; y <= height; y += height/8){
-      if (colors === 0){
-        fill(colors);
-        rect(x, y, width/8, height/8);
-        colors = 255;
-      }
-      else if (colors === 255){
-        fill(colors);
-        rect(x, y, width/8, height/8);
-        colors = 0;
-      }
+  for (x = 0; x <= width; x += 10){
+    for (y = 0; y <= height; y += 10){
+      fill(random(255), random(255), random(255));
+      rect(x, y, 10, 10);
     }
   }
 }
