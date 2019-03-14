ddf69556a86038a08d2263493892ffee4d37a1f5
Woodsie2001 <connerw306@gmail.com>
2019-03-14 10:43:53 -0600

    Done

--- Perlin Noise/cs30-p5js-template-master/sketch.js
+++ Perlin Noise/cs30-p5js-template-master/sketch.js
@@ -5,19 +5,32 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-function generateTerrain(){
-  for (let x = 0; x <= width; x += random){
-    for (let y = 0; y <= height; y += random){
-      rect(10, 10, x, y);
-    }
-  }
-}
+let inc = 0.01;
+let start = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  generateTerrain();
+  noStroke();
 }
 
 function draw() {
-  background(220);
   generateTerrain();
+}
+
+
+function generateTerrain() {
+
+  let yOff = 0;
+  let xoff = start;
+  for (let x = 0; x < width; x++) {
+    let y = noise(xoff) * height;
+    vertex(x,y);
+    xoff += inc;
+
+    fill(0);
+    //fill(0);
+    rect(x, y, 0.1, height - y); x += 1;
+  }
+  start + inc;
 }
