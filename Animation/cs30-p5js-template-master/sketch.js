fbb990d532efbf93e5bcdc3b532cac88d31513e6
Woods0402075 <0402075@sps-student.sk.ca>
2019-03-21 10:42:54 -0600

    Work

--- Animation/cs30-p5js-template-master/sketch.js
+++ Animation/cs30-p5js-template-master/sketch.js
@@ -5,17 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let Speed1 = 0;
+let Speed = 0;
 let pinImages = [];
+let pinImages1 = [];
 let counter = 0;
 let speed = 1;
-  //1 - left
-  //2 - right
+
 function preload(){
   for (let i = 1; i < 6; i++){
     pinImages.push(loadImage('assets/Walking Right ' + i + '.png', 100,100));
   }
+  for (let j = 1; j < 6; j++){
+    pinImages1.push(loadImage('assets/Walking Left' + j + '.png', 100, 100));
+  }
 }
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -23,11 +29,21 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  image(pinImages[counter],width/2,height/2);
-  if (frameCount % 2 === 0){
-    counter ++;
-    if (counter > 4) counter = 0;
+  background(255);
+  if (key === "ArrowRight"){
+    Speed ++;
+    image(pinImages[counter],Speed,height/2, 100, 200);
+    if (frameCount % 2 === 0){
+      counter ++;
+      if (counter > 4) counter = 0;
+    }
   }
-  
+  if (key === "ArrowLeft"){
+    Speed1--;
+    image(pinImages1[counter], Speed1, height/2, 100, 200);
+    if (frameCount % 2 === 0){
+      counter ++;
+      if (counter > 4) counter = 0;
+    }
+  }
 }
