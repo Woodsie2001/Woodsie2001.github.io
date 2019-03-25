0861fef51cae3342f5729348af18a5c5a797acd0
Woods0402075 <0402075@sps-student.sk.ca>
2019-03-25 10:57:08 -0600

    Almost Done

--- Animation/cs30-p5js-template-master/sketch.js
+++ Animation/cs30-p5js-template-master/sketch.js
@@ -5,18 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let Speed1 = 0;
 let Speed = 0;
 let pinImages = [];
 let counter = 0;
-let speed = 1;
+let Speed1 = 0;
 
 function preload() {
   for (let i = 1; i < 6; i++) {
     pinImages.push(loadImage('assets/Walking Right ' + i + '.png', 100, 100));
   }
-  for (let j = 1; j < 6; j++) {
+  for (let j = 1; j < 6; j++){
     pinImages.push(loadImage('assets/Walking Left ' + j + '.png', 100, 100));
+  }
+  for (let z = 1; z < 6; z++){
+    pinImages.push(loadImage('assets/Walking Up ' + z + '.png', 100, 100));
+  }
+  for (let q = 1; q < 6; q++){
+    pinImages.push(loadImage('assets/Walking Down ' + q + '.png', 100, 100));
   }
 }
 
@@ -30,9 +35,9 @@ function setup() {
 function draw() {
   frameRate(40);
   background(255);
-  image(pinImages[counter], Speed, height / 2, 100, 200);
+  image(pinImages[counter], Speed, Speed1, 100, 200);
 
-  if (key === "ArrowRight") {
+  if (key === "ArrowRight"){
     Speed++;
     if (frameCount % 2 === 0) {
       counter ++;
@@ -44,6 +49,20 @@ function draw() {
     if (frameCount % 2 === 0){
       counter ++;
       if (counter > 9) counter = 5;
+    }
+  }
+  if (key === "ArrowUp"){
+    Speed1 --;
+    if (frameCount % 2 === 0){
+      counter ++;
+      if (counter > 14) counter = 10;
+    }
+  }
+  if (key === "ArrowDown"){
+    Speed1 ++;
+    if (frameCount % 2 === 0){
+      counter ++; 
+      if (counter > 19) counter = 15
     }
   }
 }
