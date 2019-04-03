4d54d0054bcb93be0f74f1cb09d99e5402651afa
Woods0402075 <0402075@sps-student.sk.ca>
2019-04-03 10:58:32 -0600

    Stuff

--- Object-Oriented Programming Day 3/cs30-p5js-template-master/sketch.js
+++ Object-Oriented Programming Day 3/cs30-p5js-template-master/sketch.js
@@ -1,63 +1,75 @@
-
 let m;
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  m = new Mover(width/2, height/2);
+  m = new Mover(width/2,height/2);
 }
 
-
 function draw() {
-  //background(220);
   m.move();
   m.display();
-
 }
 
 class Mover{
+  //constructor and properties
   constructor(x_, y_){
-    this.size  = 10;
+    this.size = 10;
     this.position = createVector(x_, y_);
     this.velocity = createVector(0,0);
-    this.acceleration = createVector(0, 0);
+    this.acceleration = createVector(0,0);
   }
   move(){
-    this.acceleration = p5.Vector.random(20).mult(2);
+    this.acceleration = p5.Vector.random2D().mult(2);
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
-    this.velocity.limits(5);
+    this.velocity.limit(5);
+    this.edges();
+  }
+
+  edges(){
+    if(this.position.x < 0)this.position.x = width;
+    if(this.position.x > width)this.position.x = 0;
+
+    if(this.position.y < 0)this.position.y = height;
+    if(this.position.y > height)this.position.y = 0;
   }
 
   display(){
     ellipseMode(CENTER);
     push();
     translate(this.position.x, this.position.y);
-    ellipse(0, 0, this.size, this.size);
+    ellipse(0,0,this.size, this.size);
     pop();
-
   }
 }
 
-// let position; 
+
+
+// let position;
 // let velocity;
 // let gravity;
+// position = createVector(width/2, height/2);
+//   velocity = createVector(random(-5,5),random(-5,-1)) ;
+//   gravity = createVector(0,0.08);
+//    push();
+//   translate(width/2,height/2);
+//   let mouse = createVector(mouseX,mouseY);
+//   let center = createVector(width/2,height/2);
 
-// push();
-// translate(width/2, height/2);
-// let mouse = createVector(mouseX, mouseY);
-// let center = createVector(width/2, height/2);
+  
 
-// let towardsMouse = mouse.sub(center);
-// towardsMouse.normalize();
-// towardsMouse.mult(50);
-// line(0, 0, towardsMouse.x, towardsMouse.y);
+//   let towardsMouse = mouse.sub(center);
+//   //towardsMouse.limit(100);
+//   towardsMouse.normalize();
+//   towardsMouse.mult(50);
+//   line(0,0,towardsMouse.x, towardsMouse.y);
+  
+//   let m = towardsMouse.mag();
+//   textSize(30);
+//   text(m, 0, 50);
+  
+//   pop();
 
-// let m = towardsMouse.mag();
-// textSize(30);
-// text(m,0,50);
-
-// pop();
-// velocity.add(gravity);
-// position.add(velocity);
-// ellipse(position.x, position.y, 40,40);
+  
+  // velocity.add(gravity);
+  // position.add(velocity);
+  // ellipse(position.x,position.y, 40,40);
