9ff0d7265b6fa2363052f79074539e535bcdecbf
Conner <connerw306@gmail.com>
2019-05-01 09:20:00 -0600

    Work

--- Puzzle Game/cs30-p5js-template-master/sketch.js
+++ Puzzle Game/cs30-p5js-template-master/sketch.js
@@ -1,19 +1,29 @@
-let winningb = 10;
-let winningw = 10;
+// Puzzle Game
+// Conner Woods
+// May 1st, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let winB = 10;
+let winW = 10;
 let current_col, current_row;
 const NUM_ROWS = 4;
 const NUM_COLS = 5;
 let rectWidth, rectHeight;
+// Setting Variables
 
 let gridData = [[0,   255, 0,   255, 0],
                [ 255, 0,   255, 0,   255],
                [ 0,   255, 0,   255, 0],
                [ 255, 0,   255, 0,   255]];
+               // Making Grid
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectWidth = windowWidth / NUM_COLS;
   rectHeight = windowHeight / NUM_ROWS;
+  //Settting Size
 }
 
 function drawGrid() {
@@ -21,6 +31,7 @@ function drawGrid() {
     for (let x = 0; x < NUM_COLS; x++) {
       fill(gridData[y][x]);
       rect(x * rectWidth, y * rectHeight, rectWidth, rectHeight);
+      //Drawing Grid
     }
   }
 }
@@ -29,34 +40,39 @@ function flip(col, row) {
   if (col >= 0 && col < NUM_COLS && row >= 0 && row < NUM_ROWS) {
     if (gridData[row][col] === 0) {
       gridData[row][col] = 255;
-      winningb -= 1;
-      winningw += 1;
+      winB -= 1;
+      winW += 1;
     }
     else {
       gridData[row][col] = 0;
-      winningw -= 1;
-      winningb += 1;
+      winW -= 1;
+      winB += 1;
     }
   }
 }
 
-function win(){
-  if (winningb === 20 || winningw === 20){
-    textAllign(CENTER);
-    print("You Win", width/2, height/2);
-  } 
+function ifWin() {
+  if (winB === 20 || winW === 20) {
+    textSize(100);
+    textAlign(CENTER);
+    fill(255, 0, 0);
+    text("You Win!!", width/2, height/2);
+    //Determining if you win
+  }
 }
 
 function determineActiveSquare() {
   current_col = int(mouseX / rectWidth);
   current_row = int(mouseY / rectHeight);
   print(current_col, current_row);
+  //Finding the mouse
 }
 
 function draw() {
   drawGrid();
   determineActiveSquare();
-  win();
+  ifWin();
+  //Declaring functions
 }
 
 function mousePressed() {
@@ -66,8 +82,10 @@ function mousePressed() {
     flip(current_col, current_row + 1);
     flip(current_col, current_row - 1);
     flip(current_col, current_row);
+    //Plus Sign
   }
   if (mouseButton === RIGHT) {
     flip(current_col, current_row);
+    //Cheater Cheater
   }
 }
