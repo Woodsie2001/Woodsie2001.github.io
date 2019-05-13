e04e72182148bf336d65a5de567529f45c969f7c
Conner <connerw306@gmail.com>
2019-05-13 11:02:49 -0600

    Work

--- Art Stuff/cs30-p5js-template-master/sketch.js
+++ Art Stuff/cs30-p5js-template-master/sketch.js
@@ -1,6 +1,35 @@
-// Project Title
-// Your Name
-// Date
-//
+// Art Project
+// Conner Woods
+// May 2nd
+
+// My original Idea for this project was to make something neon as i did which took a long time and to create something with the
+// squares i did use the demo code as some help to get me going as it was what i wanted to create.
+
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    rectMode(CENTER);
+    //Centering Shapes
+  }
+  
+  function draw() {
+    randomSeed(4.9092);
+    rectangles(width/2,height/3, width* .6, height * .4);
+  }
+  
+  function rectangles(x,y,sideLength){
+    if(sideLength > 5){x
+      fill(random(255),random(255),0,200);
+      //Random Fill Values
+      rect(x,y,sideLength,sideLength);
+      let half = sideLength/2;
+      rectangles(x-half,y-half,half);  
+      rectangles(x-half,y+half,half);  
+      rectangles(x+half,y-half,half);  
+      rectangles(x+half,y+half,half);  
+      //Drawing Rectangles 
+    }
+  
+  }
--- Sorting/cs30-p5js-template-master/sketch.js
+++ Sorting/cs30-p5js-template-master/sketch.js
@@ -1,65 +1,33 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
 
-//selection sort
-
-let values = [];
-const ARRAY_SIZE = 1000;
 
 function setup() {
-  noCanvas();
-  noLoop();
-  populateArray();
-}
-
-function populateArray(){
-  //fill our array with 1000 random numbers from 1-1000
-  for (let i = 0; i< ARRAY_SIZE; i++){
-    values.push(int(random(1000)));
-  }
-}
-
-function binarySearch(n){
-  while (values.length > 1){
-    let index = int(values.length/2);
-    if (values[index]===n){
-      return true;
-    }
-    else{
-      if (values[index]>n){
-        //get rid of the larger numbers
-        values.splice(n);
-      }
-      else{
-        //get rid of the smaller numbers
-        values.splice(0,int(values.length/2));
-      }
-    }
-  }
-  return false;
-}
-
-function selectionSort(){
-  //one character at a time, find the minimum value and swap
-  for (let index = 0; index < values.length-1; index++){
-    let minimum = values[index];
-    let minimumLoc = index;
-    for (let checkIndex = index+1; checkIndex < values.length; checkIndex++){
-      let cur = values[checkIndex];
-      if (cur < minimum){
-        minimum = cur;
-        minimumLoc = checkIndex;
-      }
-    }
-    //swap the item at index with the item at minimumLoc
-    let temp = values[index];
-    values[index] = values[minimumLoc];
-    values[minimumLoc] = temp;
-  }
-
+  createCanvas(windowWidth, windowHeight);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+  //noFill();
 }
 
 function draw() {
-  print(values);
-  selectionSort();
-  print(values);
-  print(binarySearch(52));
+  randomSeed(4.9092);
+  background(220);
+  rectangles(width/2,height/2, width * .6, height*.6);
+}
+
+function rectangles(x,y,sideLength){
+  if(sideLength > 5){
+    fill(random(255),random(255),0,200);
+    rect(x,y,sideLength,sideLength);
+    let half = sideLength/2;
+    rotate(radians(90));
+    rectangles(x-half,y-half,half);  
+    rectangles(x-half,y+half,half); 
+    rectangles(x+half,y-half,half);  
+    rectangles(x+half,y+half,half);  
+  }
 }
