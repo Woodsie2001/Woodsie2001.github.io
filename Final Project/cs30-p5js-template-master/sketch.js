445e15f1b9134ae68157c6f65a479dda027a1f02
Conner <connerw306@gmail.com>
2019-05-27 10:56:51 -0600

    Work

--- Final Project/cs30-p5js-template-master/sketch.js
+++ Final Project/cs30-p5js-template-master/sketch.js
@@ -1,11 +1,42 @@
-// Final Project
-// Conner Woods
-// May 23rd 
+// Firework Simulator 
+// Conner
 
+let p = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight);
+	noStroke();
+}
+
+function mouseClicked() {
+	for (i = 0; i < 1000; i++) {
+		p.push(new Particle);
+	}
 }
 
 function draw() {
+	background(0, 5);
+	for (i = 0; i < p.length; i++) {
+		p[i].show();
+		p[i].move();
+	}
 }
+
+class Particle {
+	constructor() {
+		this.x = mouseX;
+		this.y = mouseY;
+		this.xSpeed = random(-5, 5);
+		this.ySpeed = random(-5, 5);
+	}
+
+	show() {
+		ellipse(this.x, this.y, 5, 5);
+	}
+
+	move() {
+		this.x = this.x + this.xSpeed;
+		this.y = this.y + this.ySpeed;
+	}
+}
+
