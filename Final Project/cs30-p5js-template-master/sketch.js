625784405b40562f201fee7804905633233935a0
Conner <connerw306@gmail.com>
2019-05-29 10:49:39 -0600

    Work

--- Final Project/cs30-p5js-template-master/sketch.js
+++ Final Project/cs30-p5js-template-master/sketch.js
@@ -8,9 +8,11 @@ function setup() {
 	noStroke();
 }
 
-function mouseClicked() {
-	for (i = 0; i < 1000; i++) {
-		p.push(new Particle);
+function keyPressed() {
+	if (key === "q"){
+		for (i = 0; i < 1000; i++) {
+			p.push(new Particle);
+		}
 	}
 }
 
@@ -24,13 +26,15 @@ function draw() {
 
 class Particle {
 	constructor() {
-		this.x = mouseX;
-		this.y = mouseY;
+		this.color = color(random(255), random(255), random(255))
+		this.x = width/2;
+		this.y = height/2;
 		this.xSpeed = random(-5, 5);
 		this.ySpeed = random(-5, 5);
 	}
 
 	show() {
+		fill(this.color);
 		ellipse(this.x, this.y, 5, 5);
 	}
 
