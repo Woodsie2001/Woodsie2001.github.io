9dc336c3a3ab4e26a2044a7f517031a807cc92da
Conner <connerw306@gmail.com>
2019-06-11 10:53:01 -0600

    Work

--- Final Project/cs30-p5js-template-master/sketch.js
+++ Final Project/cs30-p5js-template-master/sketch.js
@@ -15,6 +15,8 @@ function setup() {
 	radio = createRadio();
 	radio.option('Circles');
 	radio.option('Squares');
+	radio.option('Circles with Gravity')
+	radio.option('Squares with Gravity')
 	radio.style("color", "white");
 	radio.position(50, 50);
 	radio.style('width', '55px');
@@ -22,25 +24,30 @@ function setup() {
 	fill(255, 255, 255)
 
 	slider1 = createSlider(0, 255, 120);
-	slider1.position(25, 150);
+	slider1.position(25, 275);
 	slider1.style('width', '180px');
 
 	slider2 = createSlider(0, 255, 120);
-	slider2.position(25, 200);
+	slider2.position(25, 325);
 	slider2.style('width', '180px');
 
 	slider3 = createSlider(0, 255, 120);
-	slider3.position(25, 250);
+	slider3.position(25, 375);
 	slider3.style('width', '180px');
 	
 }
 
 function draw() {
-	background(0, 5);
-	for (let i = 0; i < p.length; i++) {
-		p[i].show();
-		p[i].move();
-	}
+		background(0, 5);
+		for (let i = 0; i < p.length; i++) {
+			p[i].show();
+			p[i].move();
+		}
+	textAlign(CENTER);
+	textSize(15);
+	text("Red Value", 75, 310);
+	text("Green Value", 75, 360);
+	text("Blue Value", 75, 410);
 }
 
 
@@ -56,6 +63,19 @@ function keyPressed() {
 				p.push(new Particle1());
 			}
 		}
+		if (radio.value() === 'Circles with Gravity'){
+			for (let i = 0; i < 500; i ++){
+				p.push(new Particle2);
+			}
+		}
+		if (radio.value() === 'Squares with Gravity'){
+			for (let i = 0; i < 500; i ++){
+				p.push(new Particle3);
+			}
+		}
+	}
+	if (key === "c"){
+		background(0);
 	}
 }
 
@@ -93,6 +113,47 @@ class Particle1 {
 	}
 
 	move() {
+		this.x = this.x + this.xSpeed;
+		this.y = this.y + this.ySpeed;
+	}
+}
+
+class Particle2 {
+	constructor() {
+		this.color = color(slider1.value(), slider2.value(), slider3.value());
+		this.x = width/2;
+		this.y = height/2;
+		this.xSpeed = random(-5, 5);
+		this.ySpeed = random(-10);
+
+	}
+
+	show() {
+		fill(this.color);
+		ellipse(this.x, this.y, 5, 6);
+	}
+	move(){
+		this.ySpeed = this.ySpeed + 0.1;
+		this.x = this.x + this.xSpeed;
+		this.y = this.y + this.ySpeed;
+	}
+}
+
+class Particle3 {
+	constructor() {
+		this.color = color(slider1.value(), slider2.value(), slider3.value());
+		this.x = width/2;
+		this.y = height/2;
+		this.xSpeed = random(-5, 5);
+		this.ySpeed = random(-10);
+	}
+	show() {
+		fill(this.color);
+		rect(this.x, this.y, 10, 10);
+	}
+
+	move() {
+		this.ySpeed = this.ySpeed + 0.1;
 		this.x = this.x + this.xSpeed;
 		this.y = this.y + this.ySpeed;
 	}
