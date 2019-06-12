928a8372b995b6f24385383e6527f474a5ffbf95
Conner <connerw306@gmail.com>
2019-06-12 10:58:52 -0600

    Stuff

--- Final Project/cs30-p5js-template-master/sketch.js
+++ Final Project/cs30-p5js-template-master/sketch.js
@@ -6,7 +6,7 @@ let radio;
 let slider1;
 let slider2;
 let slider3;
-let counter;
+let input;
 
 function setup() {
 	noStroke();
@@ -34,7 +34,9 @@ function setup() {
 	slider3 = createSlider(0, 255, 120);
 	slider3.position(25, 375);
 	slider3.style('width', '180px');
-	
+
+	input = createInput();
+	input.position(25, 430);
 }
 
 function draw() {
@@ -48,42 +50,65 @@ function draw() {
 	text("Red Value", 75, 310);
 	text("Green Value", 75, 360);
 	text("Blue Value", 75, 410);
+	text("Num Value", 75, 475);
 }
 
 
 function keyPressed() {
+	let Num = input.value();
 	if (key === " "){
-		if (radio.value() === 'Circles'){
-			for (let i  = 0; i < 500; i++) {
-				p.push(new Particle());
+		if (Num > 0){
+			if (radio.value() === 'Circles'){
+				for (let i  = 0; i < Num; i++) {
+					p.push(new Particle());
+				}
+			}
+			if (radio.value() === 'Squares'){
+				for (let i = 0; i < Num; i ++){
+					p.push(new Particle1());
+				}
+			}
+			if (radio.value() === 'Circles with Gravity'){
+				for (let i = 0; i < Num; i ++){
+					p.push(new Particle2);
+				}
+			}
+			if (radio.value() === 'Squares with Gravity'){
+				for (let i = 0; i < Num; i ++){
+					p.push(new Particle3);
+				}
 			}
 		}
-		if (radio.value() === 'Squares'){
-			for (let i = 0; i < 500; i ++){
-				p.push(new Particle1());
+		else{
+			if (radio.value() === 'Circles'){
+				for (let i  = 0; i < 500; i++) {
+					p.push(new Particle());
+				}
+			}
+			if (radio.value() === 'Squares'){
+				for (let i = 0; i < 500; i ++){
+					p.push(new Particle1());
+				}
+			}
+			if (radio.value() === 'Circles with Gravity'){
+				for (let i = 0; i < 500; i ++){
+					p.push(new Particle2);
+				}
+			}
+			if (radio.value() === 'Squares with Gravity'){
+				for (let i = 0; i < 500; i ++){
+					p.push(new Particle3);
+				}
 			}
 		}
-		if (radio.value() === 'Circles with Gravity'){
-			for (let i = 0; i < 500; i ++){
-				p.push(new Particle2);
-			}
-		}
-		if (radio.value() === 'Squares with Gravity'){
-			for (let i = 0; i < 500; i ++){
-				p.push(new Particle3);
-			}
-		}
-	}
-	if (key === "c"){
-		background(0);
 	}
 }
 
 class Particle {
 	constructor() {
 		this.color = color(slider1.value(), slider2.value(), slider3.value());
-		this.x = width/2;
-		this.y = height/2;
+		this.x = mouseX;
+		this.y = mouseY;
 		this.xSpeed = random(-5, 5);
 		this.ySpeed = random(-5, 5);
 	}
@@ -102,8 +127,8 @@ class Particle {
 class Particle1 {
 	constructor() {
 		this.color = color(slider1.value(), slider2.value(), slider3.value());
-		this.x = width/2;
-		this.y = height/2;
+		this.x = mouseX;
+		this.y = mouseY;
 		this.xSpeed = random(-5, 5);
 		this.ySpeed = random(-5, 5);
 	}
@@ -121,8 +146,8 @@ class Particle1 {
 class Particle2 {
 	constructor() {
 		this.color = color(slider1.value(), slider2.value(), slider3.value());
-		this.x = width/2;
-		this.y = height/2;
+		this.x = mouseX;
+		this.y = mouseY;
 		this.xSpeed = random(-5, 5);
 		this.ySpeed = random(-10);
 
@@ -142,8 +167,8 @@ class Particle2 {
 class Particle3 {
 	constructor() {
 		this.color = color(slider1.value(), slider2.value(), slider3.value());
-		this.x = width/2;
-		this.y = height/2;
+		this.x = mouseX;
+		this.y = mouseY;
 		this.xSpeed = random(-5, 5);
 		this.ySpeed = random(-10);
 	}
